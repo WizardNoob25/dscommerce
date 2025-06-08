509cd024f7cc91c1ada49530429959e845bb4124
diogo vieira <diogo.vcastro25@gmail.com>
2025-06-08 11:03:59 -0300

    ProducMin on Controller

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscommerce.controllers;
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +36,9 @@ public class ProductController {
     }*/
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name ="name", defaultValue = "") String name,
-            Pageable pageable) {
-        Page<ProductDTO> dtos = service.findAll(name,pageable);
+    public ResponseEntity<Page<ProductMinDTO>> findAll(@RequestParam(name ="name", defaultValue = "") String name,
+                                                       Pageable pageable) {
+        Page<ProductMinDTO> dtos = service.findAll(name,pageable);
         return ResponseEntity.ok(dtos);
     }
 
